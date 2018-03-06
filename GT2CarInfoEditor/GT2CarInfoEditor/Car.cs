d8ba4278a9a0097e15273fa75ac96d21f39deab9
pez2k <pez2k@pez2k.net>
2018-03-06 14:40:03 +0000

    CarInfoEditor: Template project.

---
+++ GT2CarInfoEditor/GT2CarInfoEditor/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2CarInfoEditor
+{
+    class Car
+    {
+        public string JPName { get; set; }
+        public string USName { get; set; }
+        public string EUName { get; set; }
+        public List<CarColour> Colours { get; set; }
+        public uint Unknown { get; set; }
+    }
+}
---
+++ GT2CarInfoEditor/GT2CarInfoEditor/CarColour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2CarInfoEditor
+{
+    class CarColour
+    {
+        public string LatinName { get; set; }
+        public string JapaneseName { get; set; }
+        public ushort Thumbnail { get; set; }
+        public byte PaletteID { get; set; }
+    }
+}
