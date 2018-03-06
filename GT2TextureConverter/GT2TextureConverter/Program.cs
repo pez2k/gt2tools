b72930030b3ce5f860c04c1ca9443e1b63db6ef0
pez2k <pez2k@pez2k.net>
2018-03-06 13:31:36 +0000

    TextureConverter: Dump the texture with each CLUT for testing. Unfortunately it requires reading of the CDO to pick the right CLUT for each face, so shelved for now.

--- GT2TextureConverter/GT2TextureConverter/Program.cs
+++ GT2TextureConverter/GT2TextureConverter/Program.cs
@@ -11,39 +11,42 @@ namespace GT2TextureConverter
         {
             using (FileStream file = new FileStream("bjjrr.cdp", FileMode.Open))
             {
-                file.Position = 0x20;
-
-                var texture = new byte[224, 256];
-                GCHandle memoryHandle = GCHandle.Alloc(texture, GCHandleType.Pinned);
-                var bitmap = new Bitmap(256, 224, 256, PixelFormat.Format8bppIndexed, memoryHandle.AddrOfPinnedObject());
-                ColorPalette palette = bitmap.Palette;
-
-                for (int i = 0; i < 224; i++)
+                for (int clut = 0; clut < 14; clut++)
                 {
-                    ushort paletteColour = file.ReadUShort();
-                    int R = paletteColour & 0x1F;
-                    int G = (paletteColour >> 5) & 0x1F;
-                    int B = (paletteColour >> 10) & 0x1F;
+                    file.Position = 0x20;
 
-                    palette.Entries[i] = Color.FromArgb(R * 8, G * 8, B * 8);
-                }
-                bitmap.Palette = palette;
+                    var texture = new byte[224, 256];
+                    GCHandle memoryHandle = GCHandle.Alloc(texture, GCHandleType.Pinned);
+                    var bitmap = new Bitmap(256, 224, 256, PixelFormat.Format8bppIndexed, memoryHandle.AddrOfPinnedObject());
+                    ColorPalette palette = bitmap.Palette;
 
-                file.Position = 0x43A0;
-                
-                for (int y = 0; y < 224; y++)
-                {
-                    for (int x = 0; x < 256; x += 2)
+                    for (int i = 0; i < 224; i++)
                     {
-                        byte pixelPair = (byte)file.ReadByte();
-                        texture[y, x] = (byte)(pixelPair & 0xF);
-                        texture[y, x + 1] = (byte)(pixelPair >> 4);
-                    }
-                }
+                        ushort paletteColour = file.ReadUShort();
+                        int R = paletteColour & 0x1F;
+                        int G = (paletteColour >> 5) & 0x1F;
+                        int B = (paletteColour >> 10) & 0x1F;
 
-                memoryHandle.Free();
-                bitmap.Save("test.bmp");
-                bitmap.Dispose();
+                        palette.Entries[i] = Color.FromArgb(R * 8, G * 8, B * 8);
+                    }
+                    bitmap.Palette = palette;
+
+                    file.Position = 0x43A0;
+
+                    for (int y = 0; y < 224; y++)
+                    {
+                        for (int x = 0; x < 256; x += 2)
+                        {
+                            byte pixelPair = (byte)file.ReadByte();
+                            texture[y, x] = (byte)((pixelPair & 0xF) + (16 * clut));
+                            texture[y, x + 1] = (byte)((pixelPair >> 4) + (16 * clut));
+                        }
+                    }
+
+                    memoryHandle.Free();
+                    bitmap.Save($"test{clut}.bmp");
+                    bitmap.Dispose();
+                }
             }
         }
     }
