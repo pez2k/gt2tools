380a774a9c390687cd9cadf6838858076b00f6d5
pez2k <pez2k@pez2k.net>
2018-03-16 21:36:08 +0000

    GT2WheelSwap: Quick and dirty TIM import into CDP.

--- GT2WheelSwap/GT2WheelSwap/Program.cs
+++ GT2WheelSwap/GT2WheelSwap/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace GT2WheelSwap
 {
@@ -11,19 +6,34 @@ namespace GT2WheelSwap
     {
         static void Main(string[] args)
         {
-            Export();
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            string filename = args[0];
+
+            if (Path.GetExtension(filename) == ".cdp")
+            {
+                Export(filename);
+            }
+            else if (Path.GetExtension(filename) == ".tim")
+            {
+                Import(filename);
+            }
         }
 
-        static void Export()
-        {
-            const uint TIM_4BPP = 0;
-            const uint TIM_INDEXED = 8;
-            const uint CDP_PALETTESTART = 0x20;
-            const uint CDP_IMAGESTART = 0x43A0;
 
-            using (FileStream cdpFile = new FileStream("bjjrr.cdp", FileMode.Open, FileAccess.Read))
+        const uint TIM_4BPP = 0;
+        const uint TIM_INDEXED = 8;
+        const uint CDP_PALETTESTART = 0x20;
+        const uint CDP_IMAGESTART = 0x43A0;
+
+        static void Export(string cdpFilename)
+        {
+            using (FileStream cdpFile = new FileStream(cdpFilename, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream timFile = new FileStream("output.tim", FileMode.Create, FileAccess.Write))
+                using (FileStream timFile = new FileStream(Path.GetFileNameWithoutExtension(cdpFilename) + "_wheel.tim", FileMode.Create, FileAccess.Write))
                 {
                     // TIM header
                     timFile.Write(new byte[] { 0x10, 0x00, 0x00, 0x00 }, 0, 4);
@@ -56,6 +66,31 @@ namespace GT2WheelSwap
                         byte[] imageRow = new byte[48 / 2]; // 48 pixels at 4BPP
                         cdpFile.Read(imageRow, 0, imageRow.Length);
                         timFile.Write(imageRow, 0, imageRow.Length);
+                    }
+                }
+            }
+        }
+
+        static void Import(string timFilename)
+        {
+            using (FileStream timFile = new FileStream(timFilename, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream cdpFile = new FileStream(Path.GetFileNameWithoutExtension(timFilename).Replace("_wheel", "") + ".cdp", FileMode.Open, FileAccess.Write))
+                {
+                    timFile.Position = 0x14;
+                    cdpFile.Position = CDP_PALETTESTART;
+                    byte[] clutData = new byte[16 * 2]; // 16 ushorts
+                    timFile.Read(clutData, 0, clutData.Length);
+                    cdpFile.Write(clutData, 0, clutData.Length);
+
+                    timFile.Position = 0x40;
+                    // Read image data from TIM
+                    for (int y = 0; y < 48; y++)
+                    {
+                        cdpFile.Position = CDP_IMAGESTART + (y * 256 / 2); // 256 pixel width at 4BPP
+                        byte[] imageRow = new byte[48 / 2]; // 48 pixels at 4BPP
+                        timFile.Read(imageRow, 0, imageRow.Length);
+                        cdpFile.Write(imageRow, 0, imageRow.Length);
                     }
                 }
             }
