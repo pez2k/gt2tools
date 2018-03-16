ae9071a3f3507b6ff0e74446baa11107c4f0dda5
pez2k <pez2k@pez2k.net>
2018-03-16 21:20:49 +0000

    GT2WheelSwap: Initial test code to dump a TIM wheel texture from a CDP.

---
+++ GT2WheelSwap/GT2WheelSwap/Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+namespace GT2WheelSwap
+{
+    public static class Extensions
+    {
+        public static uint ReadUInt(this Stream stream)
+        {
+            byte[] rawValue = new byte[4];
+            stream.Read(rawValue, 0, 4);
+            return (uint)(rawValue[3] * 256 * 256 * 256 + rawValue[2] * 256 * 256 + rawValue[1] * 256 + rawValue[0]);
+        }
+
+        public static uint ReadUInt(this byte[] array)
+        {
+            return (uint)(array[3] * 256 * 256 * 256 + array[2] * 256 * 256 + array[1] * 256 + array[0]);
+        }
+
+        public static void WriteUInt(this Stream stream, uint value)
+        {
+            stream.Write(value.ToByteArray(), 0, 4);
+        }
+
+        public static byte[] ToByteArray(this uint value)
+        {
+            byte[] byteArray = new byte[4];
+            byteArray[3] = (byte)((value / 256 / 256 / 256) % 256);
+            byteArray[2] = (byte)((value / 256 / 256) % 256);
+            byteArray[1] = (byte)((value / 256) % 256);
+            byteArray[0] = (byte)(value % 256);
+            return byteArray;
+        }
+
+        public static ushort ReadUShort(this Stream stream)
+        {
+            byte[] rawValue = new byte[2];
+            stream.Read(rawValue, 0, 2);
+            return (ushort)(rawValue[1] * 256 + rawValue[0]);
+        }
+
+        public static void WriteUShort(this Stream stream, ushort value)
+        {
+            stream.Write(value.ToByteArray(), 0, 2);
+        }
+
+        public static byte[] ToByteArray(this ushort value)
+        {
+            byte[] byteArray = new byte[2];
+            byteArray[1] = (byte)((value / 256) % 256);
+            byteArray[0] = (byte)(value % 256);
+            return byteArray;
+        }
+    }
+}
---
+++ GT2WheelSwap/GT2WheelSwap/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2WheelSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Export();
+        }
+
+        static void Export()
+        {
+            const uint TIM_4BPP = 0;
+            const uint TIM_INDEXED = 8;
+            const uint CDP_PALETTESTART = 0x20;
+            const uint CDP_IMAGESTART = 0x43A0;
+
+            using (FileStream cdpFile = new FileStream("bjjrr.cdp", FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream timFile = new FileStream("output.tim", FileMode.Create, FileAccess.Write))
+                {
+                    // TIM header
+                    timFile.Write(new byte[] { 0x10, 0x00, 0x00, 0x00 }, 0, 4);
+                    timFile.WriteUInt(TIM_4BPP + TIM_INDEXED); // TIM type flags
+
+                    // TIM CLUT header
+                    timFile.WriteUInt(12 + (16 * 2)); // Header length + CLUT size (16 ushorts)
+                    timFile.WriteUShort(0); // CLUT memory target location X
+                    timFile.WriteUShort(0); // CLUT memory target location Y
+                    timFile.WriteUShort(16); // Colours in CLUT
+                    timFile.WriteUShort(1); // Number of CLUTs
+
+                    // Read CLUT from CDP
+                    cdpFile.Position = CDP_PALETTESTART;
+                    byte[] clutData = new byte[16 * 2]; // 16 ushorts
+                    cdpFile.Read(clutData, 0, clutData.Length);
+                    timFile.Write(clutData, 0, clutData.Length);
+
+                    // TIM image header
+                    timFile.WriteUInt(12 + (48 * 48 / 2)); // Header length + image data size (48 x 48 pixels at 4BPP)
+                    timFile.WriteUShort(0); // Image memory target location X
+                    timFile.WriteUShort(0); // Image memory target location Y
+                    timFile.WriteUShort(48 / 4); // Image width / 4
+                    timFile.WriteUShort(48); // Image height - NOT divided by 4
+
+                    // Read image data from CDP
+                    for (int y = 0; y < 48; y++)
+                    {
+                        cdpFile.Position = CDP_IMAGESTART + (y * 256 / 2); // 256 pixel width at 4BPP
+                        byte[] imageRow = new byte[48 / 2]; // 48 pixels at 4BPP
+                        cdpFile.Read(imageRow, 0, imageRow.Length);
+                        timFile.Write(imageRow, 0, imageRow.Length);
+                    }
+                }
+            }
+        }
+    }
+}
