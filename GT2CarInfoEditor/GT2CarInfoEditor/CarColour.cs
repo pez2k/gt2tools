b95b572b957931a84e2998a7faca3529b9686c38
pez2k <pez2k@pez2k.net>
2018-05-10 19:01:31 +0100

    CarInfoEditor: Read colour names.

--- GT2CarInfoEditor/GT2CarInfoEditor/CarColour.cs
+++ GT2CarInfoEditor/GT2CarInfoEditor/CarColour.cs
@@ -1,4 +1,8 @@
-﻿namespace GT2.CarInfoEditor
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GT2.CarInfoEditor
 {
     using StreamExtensions;
 
@@ -26,7 +30,43 @@
             ThumbnailColour = files.JPCarInfo.ReadUShort();
 
             files.JPCarInfo.Position = index + (colourCount * 2) + colourNumber;
-            files.JPCarInfo.ReadByte(); // string index - todo
+            PaletteID = (byte)files.JPCarInfo.ReadByte();
+
+            ReadName(files, carNumber, colourNumber);
+        }
+        
+        public void ReadName(FileSet files, uint carNumber, byte colourNumber)
+        {
+            files.CarColours.Position = (carNumber * 2) + 8;
+            ushort index = files.CarColours.ReadUShort();
+            files.CarColours.Position = index + (colourNumber * 2);
+            ushort stringNumber = files.CarColours.ReadUShort();
+
+            LatinName = ReadName(files.CCLatin, stringNumber, false);
+            JapaneseName = ReadName(files.CCJapanese, stringNumber, true);
+        }
+
+        public string ReadName(Stream stream, ushort stringNumber, bool isUnicode)
+        {
+            stream.Position = stringNumber * 2;
+            ushort index = stream.ReadUShort();
+            stream.Position = index;
+
+            List<byte> stringBytes = new List<byte>();
+
+            byte currentByte = 0x00;
+
+            do
+            {
+                currentByte = (byte)stream.ReadByte();
+                if (currentByte != 0x00)
+                {
+                    stringBytes.Add(currentByte);
+                }
+            }
+            while (currentByte != 0x00);
+            
+            return (isUnicode ? Encoding.Unicode : Encoding.ASCII).GetString(stringBytes.ToArray()).TrimEnd('\0');
         }
     }
 }
