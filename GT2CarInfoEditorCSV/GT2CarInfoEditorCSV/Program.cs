1fbbff39e5f24b3f91614760a6bcf3ea3e726c72
pez2k <pez2k@pez2k.net>
2018-05-16 00:17:50 +0100

    CarInfoEditorCSV: Wrap the CarInfoEditor core code in some basic CSV dumping.

---
+++ GT2CarInfoEditorCSV/GT2CarInfoEditorCSV/Program.cs
@@ -0,0 +1,82 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.IO;
+using System.Text;
+
+namespace GT2.CarInfoEditorCSV
+{
+    using CarInfoEditor;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarList list = new CarList();
+
+            using (FileSet files = FileSet.OpenRead())
+            {
+                list.ReadFromFiles();
+            }
+
+            using (TextWriter output = new StreamWriter(File.Create("test.csv"), Encoding.UTF8))
+            {
+                using (CsvWriter csv = new CsvWriter(output))
+                {
+                    using (TextWriter colourOutput = new StreamWriter(File.Create("test2.csv"), Encoding.UTF8))
+                    {
+                        using (CsvWriter colourCsv = new CsvWriter(colourOutput))
+                        {
+                            csv.Configuration.RegisterClassMap<CarCSVMap>();
+                            csv.Configuration.QuoteAllFields = true;
+                            csv.WriteHeader<Car>();
+
+                            colourCsv.Configuration.RegisterClassMap<CarColourCSVMap>();
+                            colourCsv.Configuration.QuoteAllFields = true;
+                            colourCsv.WriteField("CarName");
+                            colourCsv.WriteHeader<CarColour>();
+
+                            foreach (Car car in list.Cars)
+                            {
+                                csv.NextRecord();
+                                csv.WriteRecord(car);
+                                
+                                foreach (CarColour colour in car.Colours)
+                                {
+                                    colourCsv.NextRecord();
+                                    colourCsv.WriteField(car.CarName);
+                                    colourCsv.WriteRecord(colour);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public sealed class CarCSVMap : ClassMap<Car>
+    {
+        public CarCSVMap()
+        {
+            Map(m => m.CarName);
+            Map(m => m.JPName);
+            Map(m => m.USName);
+            Map(m => m.EUName);
+            Map(m => m.BlockedInJapan);
+            Map(m => m.BlockedInUSA);
+            Map(m => m.BlockedInPALFIGS);
+            Map(m => m.BlockedInPALEnglish);
+        }
+    }
+
+    public sealed class CarColourCSVMap : ClassMap<CarColour>
+    {
+        public CarColourCSVMap()
+        {
+            Map(m => m.HexColour);
+            Map(m => m.PaletteID);
+            Map(m => m.JapaneseName);
+            Map(m => m.LatinName);
+        }
+    }
+}
---
+++ GT2CarInfoEditorCSV/GT2CarInfoEditorCSV/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GT2CarInfoEditorCSV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GT2CarInfoEditorCSV")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("950cf05c-3b1e-4141-8ca1-25ec7d97c99d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
