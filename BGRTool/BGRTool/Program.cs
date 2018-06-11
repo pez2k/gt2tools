6629c2be6503d2c088c32ef07f1f0f45f7de9071
pez2k <pez2k@pez2k.net>
2018-06-11 19:44:40 +0100

    BGRTool: Ultra basic command-line RGB / BGR555 conversion tool.

---
+++ BGRTool/BGRTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2.BGRTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                return;
+            }
+
+            bool toBGR = false;
+            string input = args[0];
+
+            if (args.Length == 2)
+            {
+                toBGR = (args[0].ToUpper() == "BGR");
+                input = args[1];
+            }
+
+            int value;
+            if (!int.TryParse(input, NumberStyles.HexNumber, null, out value))
+            {
+                return;
+            }
+
+            if (toBGR)
+            {
+                int R = (((value & 0xFF0000) >> 16) / 8) & 0x1F;
+                int G = ((((value & 0x00FF00) >> 8) / 8) & 0x1F) << 5;
+                int B = ((((value & 0x0000FF)) / 8) & 0x1F) << 10;
+                Console.WriteLine($"{string.Format("{0:X4}", (ushort)(R + G + B))} / {R}, {G}, {B}");
+            }
+            else
+            {
+                int R = value & 0x1F;
+                int G = (value >> 5) & 0x1F;
+                int B = (value >> 10) & 0x1F;
+                Console.WriteLine($"#{R * 8:X2}{G * 8:X2}{B * 8:X2} / {R * 8}, {G * 8}, {B * 8}");
+            }
+        }
+    }
+}
