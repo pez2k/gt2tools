8575a539329318a6aa2eec85f32ac18f0ae4ca3f
pez2k <pez2k@pez2k.net>
2018-07-07 17:29:28 +0100

    TEX2TIM: Quick edit of CDP2TIM, changing offsets to handle decompressed GT1 TEX files and commenting out import functionality.

---
+++ TEX2TIM/TEX2TIM/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.IO;
+
+namespace GT2.TEX2TIM
+{
+    using StreamExtensions;
+
+    class Program
+    {
+        const uint TIM_4BPP = 0;
+        const uint TIM_INDEXED = 8;
+        const uint TEX_PALETTESTART = 0x8060;
+        const uint TEX_IMAGESTART = 0x60;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                return;
+            }
+
+            string filename = args[0];
+            int paletteNumber = 1;
+
+            if (args.Length > 1)
+            {
+                int.TryParse(args[1], out paletteNumber);
+                if (paletteNumber < 1)
+                {
+                    paletteNumber = 1;
+                }
+            }
+
+            //if (Path.GetExtension(filename) == ".cdp" || Path.GetExtension(filename) == ".cnp")
+            //{
+                Export(filename, paletteNumber);
+            //}
+            //else if (Path.GetExtension(filename) == ".tim")
+            //{
+                //Import(filename, paletteNumber);
+            //}
+        }
+
+        static void Export(string texFilename, int paletteNumber)
+        {
+            using (FileStream texFile = new FileStream(texFilename, FileMode.Open, FileAccess.Read))
+            {
+                string timFilename = Path.GetFileNameWithoutExtension(texFilename) + "_tex" + ".tim";
+
+                using (FileStream timFile = new FileStream(timFilename, FileMode.Create, FileAccess.Write))
+                {
+                    int paletteCount = texFile.ReadByte();
+
+                    if (paletteNumber > paletteCount)
+                    {
+                        paletteNumber = 1;
+                    }
+
+                    // TIM header
+                    timFile.Write(new byte[] { 0x10, 0x00, 0x00, 0x00 }, 0, 4);
+                    timFile.WriteUInt(TIM_4BPP + TIM_INDEXED); // TIM type flags
+
+                    // TIM CLUT header
+                    timFile.WriteUInt(12 + ((16 * 2) * 16)); // Header length + CLUT size (16 ushorts) * 16 CLUTs
+                    timFile.WriteUShort(0); // CLUT memory target location X
+                    timFile.WriteUShort(0); // CLUT memory target location Y
+                    timFile.WriteUShort(16); // Colours in CLUT
+                    timFile.WriteUShort(16); // Number of CLUTs
+
+                    // Read CLUTs from TEX
+                    texFile.Position = TEX_PALETTESTART + ((paletteNumber - 1) * 0x240);
+                    byte[] clutData = new byte[16 * 2 * 16]; // 16 ushorts * 16 CLUTs
+                    texFile.Read(clutData, 0, clutData.Length);
+                    timFile.Write(clutData, 0, clutData.Length);
+
+                    // TIM image header
+                    uint imageDataLength = 256 * 256 / 2; // 256 x 256 pixels at 4BPP
+                    timFile.WriteUInt(12 + imageDataLength); // Header length + image data size
+                    timFile.WriteUShort(0); // Image memory target location X
+                    timFile.WriteUShort(0); // Image memory target location Y
+                    timFile.WriteUShort(256 / 4); // Image width / 4
+                    timFile.WriteUShort(256); // Image height - NOT divided by 4
+
+                    // Read image data from TEX
+                    texFile.Position = TEX_IMAGESTART;
+                    byte[] imageRow = new byte[imageDataLength]; // 256 x 256 pixels at 4BPP
+                    texFile.Read(imageRow, 0, imageRow.Length);
+                    timFile.Write(imageRow, 0, imageRow.Length);
+                }
+            }
+        }
+        
+        static void Import(string timFilename, int paletteNumber)
+        {
+            using (FileStream timFile = new FileStream(timFilename, FileMode.Open, FileAccess.Read))
+            {
+                string cdpFilename = Path.GetFileNameWithoutExtension(timFilename);
+                if (cdpFilename.EndsWith("_cnp"))
+                {
+                    cdpFilename = cdpFilename.Replace("_cnp", "") + ".cnp";
+                }
+                else
+                {
+                    cdpFilename = cdpFilename.Replace("_cdp", "") + ".cdp";
+                }
+
+                using (FileStream cdpFile = new FileStream(cdpFilename, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    int paletteCount = cdpFile.ReadByte();
+
+                    if (paletteNumber > paletteCount)
+                    {
+                        paletteNumber = 1;
+                    }
+
+                    timFile.Position = 0x14;
+                    byte[] clutData = new byte[16 * 2 * 16]; // 16 ushorts * 16 CLUTs
+                    timFile.Read(clutData, 0, clutData.Length);
+                    
+                    cdpFile.Position = TEX_PALETTESTART + ((paletteNumber - 1) * 0x240);
+                    cdpFile.Write(clutData, 0, clutData.Length);
+
+                    timFile.Position = 0x220;
+                    // Read image data from TIM
+                    cdpFile.Position = TEX_IMAGESTART;
+                    byte[] imageRow = new byte[256 * 224 / 2]; // 256 x 224 pixels at 4BPP
+                    timFile.Read(imageRow, 0, imageRow.Length);
+                    cdpFile.Write(imageRow, 0, imageRow.Length);
+                }
+            }
+        }
+    }
+}
---
+++ TEX2TIM/TEX2TIM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TEX2TIM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEX2TIM")]
+[assembly: AssemblyCopyright("Copyright © pez2k 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79505920-3400-44cb-8ebd-4e04fc7589a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
