cc72fc67e19bf7d55da7d201e4b385663ea96a34
pez2k <pez2k@pez2k.net>
2018-12-03 00:43:58 +0000

    SolodataEditor: Super fast implementation of file rebuilding.

--- GT2SolodataEditor/GT2SolodataEditor/Program.cs
+++ GT2SolodataEditor/GT2SolodataEditor/Program.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using CsvHelper;
 
 namespace GT2.SolodataEditor
 {
     using CarNameConversion;
-    using CsvHelper.Configuration;
     using StreamExtensions;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Dump();
+            if (args.Length == 0)
+            {
+                Dump();
+            }
+            else
+            {
+                Build();
+            }
         }
 
         static void Dump()
@@ -70,6 +73,60 @@ namespace GT2.SolodataEditor
                             csv.WriteField($"{item.Value:X4}");
                             csv.NextRecord();
                         }
+                    }
+                }
+            }
+        }
+
+        static void Build()
+        {
+            using (FileStream file = new FileStream("new_solodata.dat", FileMode.Create, FileAccess.Write))
+            {
+                using (TextReader output = new StreamReader(File.OpenRead("Menus.csv"), Encoding.UTF8))
+                {
+                    using (CsvReader csv = new CsvReader(output))
+                    {
+                        file.WriteUInt(0);
+                        uint rowCount = 0;
+                        csv.Read();
+
+                        while (csv.Read())
+                        {
+                            string key = csv.GetField(0);
+                            string value = csv.GetField(1);
+                            file.WriteUShort(ushort.Parse(value, System.Globalization.NumberStyles.HexNumber));
+                            rowCount++;
+                        }
+
+                        long filePosition = file.Position;
+                        file.Position = 0;
+                        file.WriteUInt(rowCount);
+                        file.Position = filePosition;
+                    }
+                }
+                
+                using (TextReader output = new StreamReader(File.OpenRead("Cars.csv"), Encoding.UTF8))
+                {
+                    using (CsvReader csv = new CsvReader(output))
+                    {
+                        long startPosition = file.Position;
+                        file.WriteUInt(0);
+                        uint rowCount = 0;
+                        csv.Read();
+
+                        while (csv.Read())
+                        {
+                            string key = csv.GetField(0);
+                            file.WriteUInt(CarNameConversion.ToCarID(key));
+                            string value = csv.GetField(1);
+                            file.WriteUInt(ushort.Parse(value, System.Globalization.NumberStyles.HexNumber));
+                            rowCount++;
+                        }
+
+                        long filePosition = file.Position;
+                        file.Position = startPosition;
+                        file.WriteUInt(rowCount);
+                        file.Position = filePosition;
                     }
                 }
             }
