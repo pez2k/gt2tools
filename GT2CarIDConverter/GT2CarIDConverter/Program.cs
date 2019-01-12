4382b35e59ce642757d79962fd87bb398b78e00b
pez2k <pez2k@pez2k.net>
2019-01-12 00:20:05 +0000

    CarIDConverter: Simple command-line tool to convert IDs to hex.

---
+++ GT2CarIDConverter/GT2CarIDConverter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2.CarIDConverter
+{
+    using CarNameConversion;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid number of arguments.");
+                return;
+            }
+
+            string input = args[0];
+            string plainID;
+            uint hexID;
+
+            if (input.Length < 5)
+            {
+                Console.WriteLine("Unknown parameter.");
+                return;
+            }
+            else if (input.Length > 5)
+            {
+                if (!uint.TryParse(input, System.Globalization.NumberStyles.HexNumber, null, out hexID) || hexID > "zzzzz".ToCarID())
+                {
+                    Console.WriteLine("Input is not a valid hex ID.");
+                    return;
+                }
+                plainID = hexID.ToCarName();
+            }
+            else
+            {
+                plainID = input;
+                hexID = plainID.ToCarID();
+            }
+
+            Console.WriteLine($"ID      Hex     ");
+            Console.WriteLine($"{plainID} / {hexID:X8}");
+        }
+    }
+}
