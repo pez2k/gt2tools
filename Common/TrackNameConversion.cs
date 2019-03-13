0f82c7e8c51765bce52e2a872c7d31f5332a6e29
pez2k <pez2k@pez2k.net>
2019-03-13 13:18:26 +0000

    GT2TrackIDConverter: Quick implementation of adeyblue's algorithm for generating hex IDs for track filenames, ported from Javascript.

---
+++ Common/TrackNameConversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GT2.TrackNameConversion
+{
+    public static class TrackNameConversion
+    {
+        public static string ToTrackName(this uint carID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static uint ToTrackID(this string carName)
+        {
+            uint trackID = 0;
+            char[] carNameChars = carName.ToCharArray();
+
+            foreach (char carNameChar in carNameChars)
+            {
+                byte asciiValue = (byte)carNameChar;
+                var temp1 = trackID << 6;
+                var temp2 = trackID >> 26;
+                trackID = temp1 | temp2;
+                trackID += asciiValue;
+            }
+
+            return trackID;
+        }
+    }
+}
---
+++ GT2TrackIDConverter/GT2TrackIDConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GT2.TrackIDConverter
+{
+    using TrackNameConversion;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid number of arguments.");
+                return;
+            }
+
+            string input = args[0];
+            string plainID;
+
+            if (uint.TryParse(input, System.Globalization.NumberStyles.HexNumber, null, out uint hexID))
+            {
+                plainID = hexID.ToTrackName();
+            }
+            else
+            {
+                plainID = input;
+                hexID = plainID.ToTrackID();
+            }
+
+            Console.WriteLine($"Hex      / ID");
+            Console.WriteLine($"{hexID:X8} / {plainID}");
+        }
+    }
+}
