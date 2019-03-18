202ca351e8692b161ce3e427fb4e92f1110eee89
pez2k <pez2k@pez2k.net>
2019-03-18 18:38:30 +0000

    GT3GTARExtractor: Quick tool to extract all files from a GTAR archive.

---
+++ GT3GTARExtractor/GT3GTARExtractor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GT2.StreamExtensions;
+
+namespace GT3.GTARExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No file specified.");
+                return;
+            }
+
+            string filename = args[0];
+
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                byte[] magic = new byte[4];
+                file.Read(magic);
+                if (Encoding.ASCII.GetString(magic) != "GTAR")
+                {
+                    Console.WriteLine("Not a GTAR archive.");
+                    return;
+                }
+
+                string directory = $"{Path.GetFileNameWithoutExtension(filename)}_extracted";
+                Directory.CreateDirectory(directory);
+
+                uint fileCount = file.ReadUInt();
+                uint dataStart = file.ReadUInt();
+                uint unknown = file.ReadUInt();
+
+                for (int i = 0; i < fileCount; i++)
+                {
+                    file.Position = (i * 4) + 0x10;
+                    uint start = file.ReadUInt();
+                    uint end = file.ReadUInt();
+                    uint length = end - start;
+
+                    file.Position = start + dataStart;
+                    byte[] buffer = new byte[length];
+                    file.Read(buffer);
+
+                    using (var outFile = new FileStream($"{directory}\\{i:D2}.dat", FileMode.Create, FileAccess.Write))
+                    {
+                        outFile.Write(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
