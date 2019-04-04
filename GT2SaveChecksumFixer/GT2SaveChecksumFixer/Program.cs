75390a6e2cbdae17d0c18d7f950d4627fd502ad8
pez2k <pez2k@pez2k.net>
2019-04-04 21:38:57 +0100

    GT2SaveChecksumFixer: Added tool to fix checksum on edited savegames.

---
+++ GT2SaveChecksumFixer/GT2SaveChecksumFixer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Force.Crc32;
+
+namespace GT2.SaveFixer
+{
+    using StreamExtensions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No filename provided.");
+                return;
+            }
+
+            string filename = args[0];
+
+            using (var save = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite))
+            {
+                Console.WriteLine($"Save: {filename}");
+                const int start = 0x80;
+                const int end = 0x7F1C;
+                save.Position = start;
+                byte[] buffer = new byte[end - start];
+                save.Read(buffer);
+                uint checksum = Crc32Algorithm.Compute(buffer);
+                Console.WriteLine($"Checksum: 0x{checksum:X4}");
+                save.WriteUInt(checksum);
+            }
+        }
+    }
+}
