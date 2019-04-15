14245a7d2972a34b35feaea59840966788b11201
pez2k <pez2k@pez2k.net>
2019-04-15 19:44:47 +0100

    LZSS: Clean decompression code and replace comments.

--- Common/LZSS.cs
+++ Common/LZSS.cs
@@ -1,57 +1,94 @@
 ﻿using System.IO;
+using StreamExtensions;
 
 namespace GT1.LZSS
 {
     public static class LZSS
     {
+        private const byte CompressedFlag = 0x01;
+        private const byte MinimumUncompressedDataLength = 3;
+        private const byte MultiByteDistanceFlag = 0x80;
+        private const byte MultiByteDistanceFlagMask = MultiByteDistanceFlag - 1;
+        private const byte DataLengthByteSize = 1;
+
         public static void Decompress(Stream compressed, Stream output)
         {
-            byte[] decompressedBytes = new byte[258];
-            byte isCompressedFlag = 1;
+            ushort compressionFlags = CompressedFlag;
             compressed.Position = 0;
 
             while (compressed.Position < compressed.Length)
             {
-                if (isCompressedFlag == 1) // if flag is 1 (we've run out of bits), read new flag
+                if (AllFlagsConsumed(compressionFlags))
                 {
-                    isCompressedFlag = (byte)(compressed.ReadByte() | 0x100);
+                    compressionFlags = ReadNextCompressionFlags(compressed);
                 }
 
-                if ((isCompressedFlag & 1) != 1) // not compressed, shorter than dictionary size? next byte is data
+                if (IsCurrentByteCompressed(compressionFlags))
                 {
-                    byte uncompressedByte = (byte)compressed.ReadByte();
-                    output.WriteByte(uncompressedByte);
+                    CopyCompressedBytes(compressed, output);
                 }
-                else // compressed, next byte is length - 3 (3 being dict size)
+                else
                 {
-                    ushort distance;
-                    ushort lengthOfDecompressedData = (ushort)(compressed.ReadByte() + 3);
-                    byte distanceFirstByte = (byte)compressed.ReadByte(); // next byte(s) must be step back distance
-                    if ((distanceFirstByte & 0x80) != 0x80) // 1 byte distance?
-                    {
-                        distance = (ushort)((distanceFirstByte & 0x7F) + 1);
-                    }
-                    else // 2 byte distance?
-                    {
-                        byte distanceSecondByte = (byte)compressed.ReadByte();
-                        distance = (ushort)(((distanceFirstByte & 0x7F) * 256) + distanceSecondByte + 1);
-                    }
-                    long endOfOutput = output.Position; // store end of output position
-                    output.Position = output.Position - distance; // step back the distance / offset - no bound, therefore infinitely sized window
-                    long steppedBackPosition = output.Position;
-                    for (int i = 0; i < lengthOfDecompressedData; i++) // read for the the length, looping around if length > distance
-                    {
-                        decompressedBytes[i] = (byte)output.ReadByte(); // up to 8 bit max + 3 (258)
-                        if (output.Position >= output.Length)
-                        {
-                            output.Position = steppedBackPosition;
-                        }
-                    }
-                    output.Position = endOfOutput; // write data at end of output
-                    output.Write(decompressedBytes, 0, lengthOfDecompressedData);
+                    CopyUncompressedByte(compressed, output);
                 }
-                isCompressedFlag = (byte)(isCompressedFlag >> 1); // bit handled, move on to the next one
+
+                compressionFlags = ConsumeFlag(compressionFlags);
             }
         }
+
+        private static bool AllFlagsConsumed(ushort compressionFlags) => compressionFlags == CompressedFlag;
+
+        private static ushort ReadNextCompressionFlags(Stream compressed) => (ushort)(compressed.ReadByte() | 0x100);
+
+        private static bool IsCurrentByteCompressed(ushort compressionFlags) => (compressionFlags & CompressedFlag) == CompressedFlag;
+
+        private static void CopyUncompressedByte(Stream compressed, Stream output) => output.WriteByte(compressed.ReadSingleByte());
+
+        private static void CopyCompressedBytes(Stream compressed, Stream output)
+        {
+            ushort lengthOfDecompressedData = GetUncompressedDataLength(compressed);
+            ushort distanceToStartOfUncompressedData = GetDistanceToStartOfUncompressedData(compressed);
+            DecompressBytes(output, lengthOfDecompressedData, distanceToStartOfUncompressedData);
+        }
+
+        private static ushort GetUncompressedDataLength(Stream compressed) => (ushort)(compressed.ReadByte() + MinimumUncompressedDataLength);
+
+        private static ushort GetDistanceToStartOfUncompressedData(Stream compressed)
+        {
+            ushort distance;
+            byte distanceFirstByte = compressed.ReadSingleByte();
+            if (IsMultiByteDistance(distanceFirstByte))
+            {
+                byte distanceSecondByte = compressed.ReadSingleByte();
+                distance = (ushort)(((distanceFirstByte & MultiByteDistanceFlagMask) * 256) + distanceSecondByte); // maximum 32,768, therefore 32kb window
+            }
+            else
+            {
+                distance = distanceFirstByte;
+            }
+            return (ushort)(distance + DataLengthByteSize);
+        }
+
+        private static bool IsMultiByteDistance(byte firstByte) => (firstByte & MultiByteDistanceFlag) == MultiByteDistanceFlag;
+
+        private static void DecompressBytes(Stream output, ushort lengthOfDecompressedData, ushort distanceToStartOfUncompressedData)
+        {
+            long endOfOutput = output.Position;
+            output.Position -= distanceToStartOfUncompressedData;
+            long steppedBackPosition = output.Position;
+            byte[] decompressedBytes = new byte[lengthOfDecompressedData];
+            for (int i = 0; i < lengthOfDecompressedData; i++)
+            {
+                decompressedBytes[i] = output.ReadSingleByte();
+                if (output.Position >= output.Length)
+                {
+                    output.Position = steppedBackPosition;
+                }
+            }
+            output.Position = endOfOutput;
+            output.Write(decompressedBytes, 0, lengthOfDecompressedData);
+        }
+
+        private static ushort ConsumeFlag(ushort compressionFlags) => (ushort)(compressionFlags >> 1);
     }
 }
