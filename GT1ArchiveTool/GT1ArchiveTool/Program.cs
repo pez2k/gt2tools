335ed6f8595b3a336782366baf50efdb34ef723d
pez2k <pez2k@pez2k.net>
2019-05-13 12:19:37 +0100

    GT1ArchiveTool: Add very basic cut-down GT1 archive extractor, which does not work recursively nor does it decompress files. This is so that the archive can later be rebuilt.

---
+++ GT1ArchiveTool/GT1ArchiveTool/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using StreamExtensions;
+
+namespace GT1ArchiveTool
+{
+    class Program
+    {
+        private const string Header = "LZIP";
+        private const uint Version = 1;
+        private const string Extension = ".gtz";
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Extract(args[0]);
+            }
+        }
+
+        private static void Extract(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                string directory = Path.GetFileNameWithoutExtension(filename);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                stream.Position = 0x0E;
+                ushort fileCount = stream.ReadUShort();
+
+                for (ushort i = 0; i < fileCount; i++)
+                {
+                    uint offset = stream.ReadUInt();
+                    uint size = stream.ReadUInt();
+                    uint uncompressedSize = stream.ReadUInt();
+
+                    long indexPosition = stream.Position;
+
+                    stream.Position = offset;
+
+                    byte[] buffer = new byte[size];
+                    stream.Read(buffer);
+
+                    string entryName = Path.Combine(directory, $"{i:D4}");
+
+                    if (size != uncompressedSize)
+                    {
+                        CreateGTZip(entryName, size, uncompressedSize, buffer);
+                    }
+                    else
+                    {
+                        CreateFile(entryName, buffer);
+                    }
+
+                    stream.Position = indexPosition;
+                }
+            }
+        }
+
+        private static void CreateFile(string filename, byte[] contents)
+        {
+            using (var output = new FileStream(filename, FileMode.Create, FileAccess.Write))
+            {
+                output.Write(contents);
+            }
+        }
+
+        private static void CreateGTZip(string filename, uint compressedSize, uint uncompressedSize, byte[] contents)
+        {
+            using (var output = new FileStream($"{filename}{Extension}", FileMode.Create, FileAccess.Write))
+            {
+                output.WriteCharacters(Header);
+                output.WriteUInt(Version);
+                output.WriteUInt(compressedSize);
+                output.WriteUInt(uncompressedSize);
+                output.Write(contents);
+            }
+        }
+    }
+}
