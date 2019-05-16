4e63cf8f623859885a3a4361592a6e4211329dfb
pez2k <pez2k@pez2k.net>
2019-05-16 11:07:06 +0100

    GT3HashGenerator: Add simple program to generate name hashes for GT3.

---
+++ GT3HashGenerator/GT3HashGenerator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT3.HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid number of arguments.");
+                return;
+            }
+
+            string name = args[0];
+            ulong hash = GenerateHash(name);
+            Console.WriteLine($"Hash             / Name");
+            Console.WriteLine($"{hash:X16} / {name}");
+        }
+
+        public static ulong GenerateHash(string name)
+        {
+            ulong hash = 0;
+            char[] nameChars = name.ToCharArray();
+
+            foreach (char nameChar in nameChars)
+            {
+                hash += (byte)nameChar;
+            }
+
+            foreach (char nameChar in nameChars)
+            {
+                byte asciiValue = (byte)nameChar;
+                ulong temp1 = hash << 7;
+                ulong temp2 = hash >> 57;
+                hash = temp1 | temp2;
+                hash += asciiValue;
+            }
+
+            return hash;
+        }
+    }
+}
