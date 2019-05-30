cb92002bf1e23acb280b9201c1ffaa0e54521d79
pez2k <pez2k@pez2k.net>
2019-05-30 20:04:05 +0100

    Common: Simplify StreamExtensions with BitConverter.

--- Common/StreamExtensions.cs
+++ Common/StreamExtensions.cs
@@ -20,30 +20,19 @@ namespace StreamExtensions
 
         public static uint ReadUInt(this Stream stream)
         {
-            byte[] rawValue = new byte[4];
+            byte[] rawValue = new byte[sizeof(uint)];
             stream.Read(rawValue);
-            return (uint)(rawValue[3] * 256 * 256 * 256 + rawValue[2] * 256 * 256 + rawValue[1] * 256 + rawValue[0]);
+            return rawValue.ReadUInt();
         }
 
-        public static uint ReadUInt(this byte[] array)
-        {
-            return (uint)(array[3] * 256 * 256 * 256 + array[2] * 256 * 256 + array[1] * 256 + array[0]);
-        }
+        public static uint ReadUInt(this byte[] array) => BitConverter.ToUInt32(array, 0);
 
         public static void WriteUInt(this Stream stream, uint value)
         {
-            stream.Write(value.ToByteArray(), 0, 4);
+            stream.Write(value.ToByteArray(), 0, sizeof(uint));
         }
 
-        public static byte[] ToByteArray(this uint value)
-        {
-            byte[] byteArray = new byte[4];
-            byteArray[3] = (byte)((value / 256 / 256 / 256) % 256);
-            byteArray[2] = (byte)((value / 256 / 256) % 256);
-            byteArray[1] = (byte)((value / 256) % 256);
-            byteArray[0] = (byte)(value % 256);
-            return byteArray;
-        }
+        public static byte[] ToByteArray(this uint value) => BitConverter.GetBytes(value);
 
         public static short ReadShort(this Stream stream)
         {
@@ -63,27 +52,23 @@ namespace StreamExtensions
 
         public static ushort ReadUShort(this Stream stream)
         {
-            byte[] rawValue = new byte[2];
+            byte[] rawValue = new byte[sizeof(ushort)];
             stream.Read(rawValue);
-            return (ushort)(rawValue[1] * 256 + rawValue[0]);
+            return rawValue.ReadUShort();
         }
+
+        public static ushort ReadUShort(this byte[] array) => BitConverter.ToUInt16(array, 0);
 
         public static void WriteUShort(this Stream stream, ushort value)
         {
-            stream.Write(value.ToByteArray(), 0, 2);
+            stream.Write(value.ToByteArray(), 0, sizeof(ushort));
         }
+
+        public static byte[] ToByteArray(this ushort value) => BitConverter.GetBytes(value);
 
         public static void WriteCharacters(this Stream stream, string characters)
         {
             stream.Write(Encoding.ASCII.GetBytes(characters));
-        }
-
-        public static byte[] ToByteArray(this ushort value)
-        {
-            byte[] byteArray = new byte[2];
-            byteArray[1] = (byte)((value / 256) % 256);
-            byteArray[0] = (byte)(value % 256);
-            return byteArray;
         }
 
         public static byte[] ToByteArray(this string value)
@@ -93,11 +78,18 @@ namespace StreamExtensions
 
         public static ulong ReadULong(this Stream stream)
         {
-            byte[] value = new byte[8];
+            byte[] value = new byte[sizeof(ulong)];
             stream.Read(value);
             return ReadULong(value);
         }
 
         public static ulong ReadULong(this byte[] array) => BitConverter.ToUInt64(array, 0);
+
+        public static void WriteULong(this Stream stream, ulong value)
+        {
+            stream.Write(value.ToByteArray(), 0, sizeof(ulong));
+        }
+
+        public static byte[] ToByteArray(this ulong value) => BitConverter.GetBytes(value);
     }
 }
