4603f132c1b8a3bdfa4f4038bf4778f468e3b6fe
pez2k <pez2k@pez2k.net>
2019-08-06 18:41:33 +0100

    GT2TextureEditor: Dump arbitrary filenames.

--- GT2TextureEditor/GT2TextureEditor/Program.cs
+++ GT2TextureEditor/GT2TextureEditor/Program.cs
@@ -6,11 +6,16 @@ namespace GT2.TextureEditor
     {
         static void Main(string[] args)
         {
-            using (var file = new FileStream("a2azn.cnp", FileMode.Open, FileAccess.Read))
+            Dump(args[0]);
+        }
+
+        static void Dump(string filename)
+        {
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
             {
                 var texture = new CarTexture();
                 texture.LoadFromGameFile(file, new CDPFileLayout());
-                using (var bitmap = new FileStream("a2azn.bmp", FileMode.Create, FileAccess.Write))
+                using (var bitmap = new FileStream($"{Path.GetFileNameWithoutExtension(filename)}.bmp", FileMode.Create, FileAccess.Write))
                 {
                     texture.WriteToEditableFiles(bitmap);
                 }
