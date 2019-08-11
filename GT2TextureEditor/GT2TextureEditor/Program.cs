436edd50ea1d3513807a1851759f4863e4cb76d2
pez2k <pez2k@pez2k.net>
2019-08-11 21:14:53 +0100

    GT2TextureEditor: Refactor top-level code to separate reading and writing methods.

--- GT2TextureEditor/GT2TextureEditor/Program.cs
+++ GT2TextureEditor/GT2TextureEditor/Program.cs
@@ -15,46 +15,56 @@ namespace GT2.TextureEditor
             string path = args[0];
             if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
             {
-                Build(path);
+                WriteToGameFile(path, ReadFromEditableFiles(path), new CDPFileLayout());
             }
             else
             {
-                Dump(path);
+                WriteToEditableFiles(path, LoadFromGameFile(path));
             }
         }
 
-        static void Dump(string filename)
+        static CarTexture LoadFromGameFile(string filename)
         {
             using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
             {
                 var texture = new CarTexture();
                 texture.LoadFromGameFile(file, Path.GetExtension(filename) == ".tex" ? (GameFileLayout)new TEXFileLayout() : new CDPFileLayout());
-                bool isNight = Path.GetExtension(filename) == ".cnp";
-                string outputName = Path.GetFileNameWithoutExtension(filename);
-                string outputPath = Path.Combine(Path.GetDirectoryName(filename), outputName + (isNight ? "_night" : ""));
-                Directory.CreateDirectory(outputPath);
-                using (var bitmap = new FileStream(Path.Combine(outputPath, $"{outputName}.bmp"), FileMode.Create, FileAccess.Write))
-                {
-                    texture.WriteToEditableFiles(outputPath, bitmap);
-                }
+                return texture;
             }
         }
 
-        static void Build(string directory)
+        static void WriteToEditableFiles(string filename, CarTexture texture)
+        {
+            bool isNight = Path.GetExtension(filename) == ".cnp";
+            string outputName = Path.GetFileNameWithoutExtension(filename);
+            string outputPath = Path.Combine(Path.GetDirectoryName(filename), outputName + (isNight ? "_night" : ""));
+            Directory.CreateDirectory(outputPath);
+            using (var bitmap = new FileStream(Path.Combine(outputPath, $"{outputName}.bmp"), FileMode.Create, FileAccess.Write))
+            {
+                texture.WriteToEditableFiles(outputPath, bitmap);
+            }
+        }
+
+        static CarTexture ReadFromEditableFiles(string directory)
+        {
+            string carName = Path.GetFileName(directory);
+            string carNameNoSuffix = carName.Replace("_night", "");
+            var texture = new CarTexture();
+            using (var bitmap = new FileStream(Path.Combine(carName, $"{carNameNoSuffix}.bmp"), FileMode.Open, FileAccess.Read))
+            {
+                texture.LoadFromEditableFiles(directory, bitmap);
+            }
+            return texture;
+        }
+
+        static void WriteToGameFile(string directory, CarTexture texture, GameFileLayout layout)
         {
             string carName = Path.GetFileName(directory);
             bool isNight = carName.EndsWith("_night");
             string carNameNoSuffix = carName.Replace("_night", "");
-            var layout = new CDPFileLayout();
-
             using (var file = new FileStream($"{carNameNoSuffix}.c{(isNight ? "n" : "d")}p", FileMode.Create, FileAccess.Write))
             {
                 file.Write(layout.HeaderData);
-                var texture = new CarTexture();
-                using (var bitmap = new FileStream(Path.Combine(carName, $"{carNameNoSuffix}.bmp"), FileMode.Open, FileAccess.Read))
-                {
-                    texture.LoadFromEditableFiles(directory, bitmap);
-                }
                 texture.WriteToGameFile(file, layout);
             }
         }
