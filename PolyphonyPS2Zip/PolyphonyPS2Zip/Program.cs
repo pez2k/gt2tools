854751ad09cc282a9550758a38a8193d3c72b5b8
pez2k <pez2k@pez2k.net>
2019-08-18 20:15:21 +0100

    PolyphonyPS2Zip: First implementation of decompression of GT4P-onwards compressed files. Decompiled compression source from GT6 Track Path Editor is in a class named 'ps2zip', hence the tool name.

---
+++ PolyphonyPS2Zip/PolyphonyPS2Zip/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using StreamExtensions;
+
+namespace PolyphonyPS2Zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid arguments.");
+                return;
+            }
+
+            Extract(args[0]);
+        }
+
+        static void Extract(string filename)
+        {
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                if (file.ReadUInt() != 0xFFF7EEC5)
+                {
+                    Console.WriteLine("Not a valid PS2Zip file.");
+                    return;
+                }
+                int uncompressedSize = -file.ReadInt();
+
+                using (var output = new FileStream($"decompressed_{Path.GetFileName(filename)}", FileMode.Create, FileAccess.Write))
+                {
+                    using (var decompression = new DeflateStream(file, CompressionMode.Decompress))
+                    {
+                        decompression.CopyTo(output);
+                    }
+                    if (output.Position != uncompressedSize)
+                    {
+                        Console.WriteLine($"Warning: file size {output.Position} does not match expected size of {uncompressedSize}.");
+                    }
+                }
+            }
+        }
+    }
+}
