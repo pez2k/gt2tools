95170f9ee840c5e176fde85b0484d4950e88d419
pez2k <pez2k@pez2k.net>
2019-09-19 18:24:48 +0100

    GT3DataSplitter: Use mined GT6 data to populate some value names.

--- GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/Displacement.cs
+++ GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/Displacement.cs
@@ -1,6 +1,35 @@
-﻿namespace GT3.DataSplitter
+﻿using CsvHelper.Configuration;
+using System.Runtime.InteropServices;
+
+namespace GT3.DataSplitter
 {
-    public class Displacement : GenericEngineUpgrade
+    public class Displacement : CsvDataStructure<DisplacementData, DisplacementCSVMap>
     {
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x18
+    public struct DisplacementData
+    {
+        public ulong Part;
+        public ulong Car;
+        public byte Stage;
+        public byte TorqueModifier;
+        public byte TorqueModifier2;
+        public byte TorqueModifier3;
+        public uint Price;
+    }
+
+    public sealed class DisplacementCSVMap : ClassMap<DisplacementData>
+    {
+        public DisplacementCSVMap()
+        {
+            Map(m => m.Part).TypeConverter(Utils.IdConverter);
+            Map(m => m.Car).TypeConverter(Utils.IdConverter);
+            Map(m => m.Stage);
+            Map(m => m.TorqueModifier);
+            Map(m => m.TorqueModifier2);
+            Map(m => m.TorqueModifier3);
+            Map(m => m.Price);
+        }
     }
 }
--- GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/PropellerShaft.cs
+++ GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/PropellerShaft.cs
@@ -1,10 +1,43 @@
-﻿namespace GT3.DataSplitter
+﻿using CsvHelper.Configuration;
+using System.Runtime.InteropServices;
+
+namespace GT3.DataSplitter
 {
-    public class PropellerShaft : NamedDataStructure
+    public class PropellerShaft : CsvDataStructure<PropellerShaftData, PropellerShaftCSVMap>
     {
-        public PropellerShaft()
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x20
+    public struct PropellerShaftData
+    {
+        public ulong Part;
+        public ulong Car;
+        public byte Stage;
+        public byte EngineBraking;
+        public byte IWheelF;
+        public byte IWheelR;
+        public byte IPropF;
+        public byte IPropR;
+        public ushort Unknown1;
+        public uint Unknown2;
+        public uint Price;
+    }
+
+    public sealed class PropellerShaftCSVMap : ClassMap<PropellerShaftData>
+    {
+        public PropellerShaftCSVMap()
         {
-            Size = 0x20;
+            Map(m => m.Part).TypeConverter(Utils.IdConverter);
+            Map(m => m.Car).TypeConverter(Utils.IdConverter);
+            Map(m => m.Stage);
+            Map(m => m.EngineBraking);
+            Map(m => m.IWheelF);
+            Map(m => m.IWheelR);
+            Map(m => m.IPropF);
+            Map(m => m.IPropR);
+            Map(m => m.Unknown1);
+            Map(m => m.Unknown2);
+            Map(m => m.Price);
         }
     }
 }
