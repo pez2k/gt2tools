7a1edce3d12c02e71497e4e035588a637811b1a0
pez2k <pez2k@pez2k.net>
2019-09-19 18:30:29 +0100

    GT3DataSplitter: CSV map generic engine upgrades.

--- GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/Displacement.cs
+++ GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/Displacement.cs
@@ -1,35 +1,6 @@
-﻿using CsvHelper.Configuration;
-using System.Runtime.InteropServices;
-
-namespace GT3.DataSplitter
+﻿namespace GT3.DataSplitter
 {
-    public class Displacement : CsvDataStructure<DisplacementData, DisplacementCSVMap>
+    public class Displacement : GenericEngineUpgrade
     {
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x18
-    public struct DisplacementData
-    {
-        public ulong Part;
-        public ulong Car;
-        public byte Stage;
-        public byte TorqueModifier;
-        public byte TorqueModifier2;
-        public byte TorqueModifier3;
-        public uint Price;
-    }
-
-    public sealed class DisplacementCSVMap : ClassMap<DisplacementData>
-    {
-        public DisplacementCSVMap()
-        {
-            Map(m => m.Part).TypeConverter(Utils.IdConverter);
-            Map(m => m.Car).TypeConverter(Utils.IdConverter);
-            Map(m => m.Stage);
-            Map(m => m.TorqueModifier);
-            Map(m => m.TorqueModifier2);
-            Map(m => m.TorqueModifier3);
-            Map(m => m.Price);
-        }
     }
 }
--- GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/GenericEngineUpgrade.cs
+++ GT3DataSplitter/GT3DataSplitter/DataStructures/ParamDB/GenericEngineUpgrade.cs
@@ -1,10 +1,35 @@
-﻿namespace GT3.DataSplitter
+﻿using CsvHelper.Configuration;
+using System.Runtime.InteropServices;
+
+namespace GT3.DataSplitter
 {
-    public class GenericEngineUpgrade : NamedDataStructure
+    public class GenericEngineUpgrade : CsvDataStructure<GenericEngineUpgradeData, GenericEngineUpgradeCSVMap>
     {
-        public GenericEngineUpgrade()
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x18
+    public struct GenericEngineUpgradeData
+    {
+        public ulong Part;
+        public ulong Car;
+        public byte Stage;
+        public byte TorqueModifier;
+        public byte TorqueModifier2;
+        public byte TorqueModifier3;
+        public uint Price;
+    }
+
+    public sealed class GenericEngineUpgradeCSVMap : ClassMap<GenericEngineUpgradeData>
+    {
+        public GenericEngineUpgradeCSVMap()
         {
-            Size = 0x18;
+            Map(m => m.Part).TypeConverter(Utils.IdConverter);
+            Map(m => m.Car).TypeConverter(Utils.IdConverter);
+            Map(m => m.Stage);
+            Map(m => m.TorqueModifier);
+            Map(m => m.TorqueModifier2);
+            Map(m => m.TorqueModifier3);
+            Map(m => m.Price);
         }
     }
 }
