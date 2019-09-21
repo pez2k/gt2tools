f59bbd83f74dc4bf348727a727f6bca307adbc31
pez2k <pez2k@pez2k.net>
2019-09-21 17:23:10 +0100

    GT3GameConfigEditor: Refactor prize list dumping and discard structural data that should be regenerated on import.

--- GT3GameConfigEditor/GT3GameConfigEditor/Prizes.cs
+++ GT3GameConfigEditor/GT3GameConfigEditor/Prizes.cs
@@ -1,12 +1,37 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using CsvHelper;
+using CsvHelper.Configuration;
 using StreamExtensions;
 
 namespace GT3.GameConfigEditor
 {
     static class Prizes
     {
+        private struct PrizeData
+        {
+            public string Event;
+            public uint PrizeID;
+            public uint Unknown;
+            public uint Colour;
+            public uint Unknown2;
+            public string PrizeCar;
+        }
+
+        private sealed class PrizeCSVMap : ClassMap<PrizeData>
+        {
+            public PrizeCSVMap()
+            {
+                Map(m => m.Event);
+                Map(m => m.PrizeID);
+                Map(m => m.Unknown);
+                Map(m => m.Colour);
+                Map(m => m.Unknown2);
+                Map(m => m.PrizeCar);
+            }
+        }
+
         public static void Dump(Stream file, string directory, int fileNumber)
         {
             uint structureCount = file.ReadUInt();
@@ -18,19 +43,8 @@ namespace GT3.GameConfigEditor
                     using (var csv = new CsvWriter(output))
                     {
                         csv.Configuration.QuoteAllFields = true;
-                        csv.WriteField("PrizeCount");
-                        csv.WriteField("Unknown");
-                        csv.WriteField("Event");
-                        csv.WriteField("Unknown2");
-                        csv.WriteField("Unknown3");
-                        csv.WriteField("Unknown4");
-                        csv.WriteField("Unknown5");
-                        csv.WriteField("PrizeID");
-                        csv.WriteField("Unknown6");
-                        // prize
-                        csv.WriteField("Colour");
-                        csv.WriteField("Unknown7");
-                        csv.WriteField("PrizeCar");
+                        csv.Configuration.RegisterClassMap<PrizeCSVMap>();
+                        csv.WriteHeader<PrizeData>();
                         csv.NextRecord();
 
                         for (int i = 0; i < structureCount; i++)
@@ -52,58 +66,32 @@ namespace GT3.GameConfigEditor
                             uint structureSize = nextStructurePos - structurePos;
 
                             file.Position = structurePos;
-                            
-                            uint structType = file.ReadUInt(); // always 0x0C?
+
+                            file.ReadUInt(); // always 0x0C
                             uint prizeCount = file.ReadUInt();
-                            uint unknown = file.ReadUInt();
+                            uint prizeOffsetsOffset = file.ReadUInt(); // offset to start of prize indexes
                             string eventName = file.ReadCharacters();
-                            long gap = file.Position % 4;
-                            if (gap > 0)
+                            file.Position = structurePos + prizeOffsetsOffset;
+                            var prizeOffsets = new uint[prizeCount];
+                            for (uint prizeNumber = 0; prizeNumber < prizeCount; prizeNumber++)
                             {
-                                file.Position += 4 - gap;
-                            }
-                            uint unknown2 = file.ReadUInt();
-                            uint unknown3 = 0;
-                            uint unknown4 = 0;
-                            uint unknown5 = 0;
-                            if (prizeCount > 1)
-                            {
-                                unknown3 = file.ReadUInt();
-                            }
-                            if (prizeCount > 2)
-                            {
-                                unknown4 = file.ReadUInt();
-                            }
-                            if (prizeCount > 3)
-                            {
-                                unknown5 = file.ReadUInt();
+                                prizeOffsets[prizeNumber] = file.ReadUInt();
                             }
 
-                            for (int j = 0; j < prizeCount; j++)
+                            for (uint prizeNumber = 0; prizeNumber < prizeCount; prizeNumber++)
                             {
-                                uint prizeID = file.ReadUInt();
-                                uint unknown6 = file.ReadUInt(); // always 16?
-                                uint colour = file.ReadUInt();
-                                uint unknown7 = file.ReadUInt(); // always 1?
-                                string prizeCar = file.ReadCharacters();
-                                gap = file.Position % 4;
-                                if (gap > 0)
-                                {
-                                    file.Position += 4 - gap;
-                                }
+                                file.Position = structurePos + prizeOffsets[prizeNumber];
 
-                                csv.WriteField(prizeCount);
-                                csv.WriteField(unknown);
-                                csv.WriteField(eventName);
-                                csv.WriteField(unknown2);
-                                csv.WriteField(unknown3);
-                                csv.WriteField(unknown4);
-                                csv.WriteField(unknown5);
-                                csv.WriteField(prizeID);
-                                csv.WriteField(unknown6);
-                                csv.WriteField(colour == 0xFFFFFFFF ? 0 : colour);
-                                csv.WriteField(unknown7);
-                                csv.WriteField(prizeCar);
+                                var data = new PrizeData();
+                                data.Event = eventName;
+                                data.PrizeID = file.ReadUInt();
+                                data.Unknown = file.ReadUInt(); // always 0x10?
+                                uint colour = file.ReadUInt(); // 0xFF FF FF FF if unset
+                                data.Colour = colour == 0xFFFFFFFF ? 0 : colour;
+                                data.Unknown2 = file.ReadUInt(); // always 1?
+                                data.PrizeCar = file.ReadCharacters();
+
+                                csv.WriteRecord(data);
                                 csv.NextRecord();
                             }
                         }
