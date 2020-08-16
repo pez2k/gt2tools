939559677ec14eac3237d04cf6da4441a0ab95bd
pez2k <pez2k@pez2k.net>
2020-08-16 11:12:27 +0100

    GT1MenuTextureEditor: Tidy up code a bit.

--- GT1MenuTextureEditor/GT1MenuTextureEditor/Program.cs
+++ GT1MenuTextureEditor/GT1MenuTextureEditor/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -19,42 +18,34 @@ namespace GT1.MenuTextureEditor
             }
 
             string path = args.Last();
-            bool isDirectory = File.GetAttributes(path).HasFlag(FileAttributes.Directory);
-            if (isDirectory) {
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)) {
                 Extract(path);
             }
         }
 
         private static void Extract(string directory) {
-            const int ColumnWidth = 256;
-            const int RightColumnWidth = 128;
+            const int PaletteSize = 16;
+            const int ColumnWidth = 256 / 2;
+            const int RightColumnWidth = 128 / 2;
             const int TopRowHeight = 256;
             const int BottomRowHeight = 224;
-            const int BitmapWidth = ColumnWidth + ColumnWidth + RightColumnWidth;
+            const int BitmapWidth = (ColumnWidth + ColumnWidth + RightColumnWidth) * 2;
             const int BitmapHeight = TopRowHeight + BottomRowHeight;
+
             string[] filenames = Directory.EnumerateFiles(directory).ToArray();
             Assert(filenames.Length == 7, $"Expected 7 files in folder, found {filenames.Length}.");
-            Assert(new FileInfo(filenames[0]).Length == 32);
-            Assert(new FileInfo(filenames[1]).Length == ColumnWidth * TopRowHeight / 2);
-            Assert(new FileInfo(filenames[2]).Length == ColumnWidth * TopRowHeight / 2);
-            Assert(new FileInfo(filenames[3]).Length == RightColumnWidth * TopRowHeight / 2);
-            Assert(new FileInfo(filenames[4]).Length == ColumnWidth * BottomRowHeight / 2);
-            Assert(new FileInfo(filenames[5]).Length == ColumnWidth * BottomRowHeight / 2);
-            Assert(new FileInfo(filenames[6]).Length == RightColumnWidth * BottomRowHeight / 2);
+            Assert(new FileInfo(filenames[0]).Length == PaletteSize * 2);
+            Assert(new FileInfo(filenames[1]).Length == ColumnWidth * TopRowHeight);
+            Assert(new FileInfo(filenames[2]).Length == ColumnWidth * TopRowHeight);
+            Assert(new FileInfo(filenames[3]).Length == RightColumnWidth * TopRowHeight);
+            Assert(new FileInfo(filenames[4]).Length == ColumnWidth * BottomRowHeight);
+            Assert(new FileInfo(filenames[5]).Length == ColumnWidth * BottomRowHeight);
+            Assert(new FileInfo(filenames[6]).Length == RightColumnWidth * BottomRowHeight);
             
             var texture = new byte[BitmapHeight, BitmapWidth];
             GCHandle memoryHandle = GCHandle.Alloc(texture, GCHandleType.Pinned);
             using (var bitmap = new Bitmap(BitmapWidth, BitmapHeight, BitmapWidth, PixelFormat.Format4bppIndexed, memoryHandle.AddrOfPinnedObject()))
             {
-                /*
-                for (ushort x = 0; x < BitmapWidth; x += 2)
-                {
-                    for (ushort y = 0; y < BitmapHeight; y++)
-                    {
-                        texture[y, x / 2] = (byte)((bitmapData[x, y] << 4) + (bitmapData[x + 1, y] & 0xF));
-                    }
-                }*/
-
                 for (int fileNumber = 0; fileNumber < filenames.Length; fileNumber++)
                 {
                     using (var stream = new FileStream(filenames[fileNumber], FileMode.Open, FileAccess.Read))
@@ -62,7 +53,7 @@ namespace GT1.MenuTextureEditor
                         if (fileNumber == 0)
                         {
                             ColorPalette palette = bitmap.Palette;
-                            for (int i = 0; i < 16; i++)
+                            for (int i = 0; i < PaletteSize; i++)
                             {
                                 ushort paletteColour = stream.ReadUShort();
                                 int R = paletteColour & 0x1F;
@@ -75,20 +66,17 @@ namespace GT1.MenuTextureEditor
                         }
                         else
                         {
-                            int tileWidth = (fileNumber % 3 == 0 ? RightColumnWidth : ColumnWidth) / 2;
+                            int tileWidth = fileNumber % 3 == 0 ? RightColumnWidth : ColumnWidth;
                             int tileHeight = fileNumber > 3 ? BottomRowHeight : TopRowHeight;
-                            int startX = (fileNumber - 1) % 3 * 256 / 2;
-                            int startY = fileNumber > 3 ? 256 : 0;
+                            int startX = (fileNumber - 1) % 3 * ColumnWidth;
+                            int startY = fileNumber > 3 ? TopRowHeight : 0;
                             for (int y = startY; y < startY + tileHeight; y++)
                             {
                                 var row = new byte[tileWidth];
                                 stream.Read(row);
                                 for (int x = 0; x < tileWidth; x++)
                                 {
-                                    byte pixelPair = row[x];
-                                    byte b1 = (byte)(pixelPair & 0xF);
-                                    byte b2 = (byte)(pixelPair >> 4);
-                                    texture[y, startX + x] = (byte)((b1 << 4) + (b2 & 0xF));
+                                    texture[y, startX + x] = SwapByteHalves(row[x]);
                                 }
                             }
                         }
@@ -103,6 +91,8 @@ namespace GT1.MenuTextureEditor
                 }
             }
         }
+
+        private static byte SwapByteHalves(byte input) => (byte)(((input & 0xF) << 4) + ((input >> 4) & 0xF));
 
         private static void Assert(bool condition, string message = null) {
             if (!condition)
