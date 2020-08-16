f10e4c4148805ad1b5e3d203fdcbf84ea4bab29a
pez2k <pez2k@pez2k.net>
2020-08-16 12:37:22 +0100

    GT1MenuTextureEditor: Add BMP to SLT conversion.

--- GT1MenuTextureEditor/GT1MenuTextureEditor/Program.cs
+++ GT1MenuTextureEditor/GT1MenuTextureEditor/Program.cs
@@ -10,6 +10,14 @@ namespace GT1.MenuTextureEditor
 {
     class Program
     {
+        private const int PaletteSize = 16;
+        private const int ColumnWidth = 256 / 2;
+        private const int RightColumnWidth = 128 / 2;
+        private const int TopRowHeight = 256;
+        private const int BottomRowHeight = 224;
+        private const int BitmapWidth = (ColumnWidth + ColumnWidth + RightColumnWidth) * 2;
+        private const int BitmapHeight = TopRowHeight + BottomRowHeight;
+
         static void Main(string[] args)
         {
             if (args.Length != 1)
@@ -18,20 +26,18 @@ namespace GT1.MenuTextureEditor
             }
 
             string path = args.Last();
-            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)) {
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+            {
                 Extract(path);
+            }
+            else if (Path.GetExtension(path) == ".bmp")
+            {
+                Rebuild(path);
             }
         }
 
-        private static void Extract(string directory) {
-            const int PaletteSize = 16;
-            const int ColumnWidth = 256 / 2;
-            const int RightColumnWidth = 128 / 2;
-            const int TopRowHeight = 256;
-            const int BottomRowHeight = 224;
-            const int BitmapWidth = (ColumnWidth + ColumnWidth + RightColumnWidth) * 2;
-            const int BitmapHeight = TopRowHeight + BottomRowHeight;
-
+        private static void Extract(string directory)
+        {
             string[] filenames = Directory.EnumerateFiles(directory).ToArray();
             Assert(filenames.Length == 7, $"Expected 7 files in folder, found {filenames.Length}.");
             Assert(new FileInfo(filenames[0]).Length == PaletteSize * 2);
@@ -66,10 +72,7 @@ namespace GT1.MenuTextureEditor
                         }
                         else
                         {
-                            int tileWidth = fileNumber % 3 == 0 ? RightColumnWidth : ColumnWidth;
-                            int tileHeight = fileNumber > 3 ? BottomRowHeight : TopRowHeight;
-                            int startX = (fileNumber - 1) % 3 * ColumnWidth;
-                            int startY = fileNumber > 3 ? TopRowHeight : 0;
+                            (int tileWidth, int tileHeight, int startX, int startY) = GetTileInfo(fileNumber);
                             for (int y = startY; y < startY + tileHeight; y++)
                             {
                                 var row = new byte[tileWidth];
@@ -92,9 +95,67 @@ namespace GT1.MenuTextureEditor
             }
         }
 
+        private static void Rebuild(string bitmapPath)
+        {
+            string outputDirectory = Path.GetFileNameWithoutExtension(bitmapPath) + ".slt";
+            Directory.CreateDirectory(outputDirectory);
+            byte[] bitmapData;
+            using (var bitmapFile = new FileStream(bitmapPath, FileMode.Open, FileAccess.Read))
+            {
+                using (var bitmap = new Bitmap(bitmapFile))
+                {
+                    Assert(bitmap.Width == BitmapWidth && bitmap.Height == BitmapHeight && bitmap.PixelFormat == PixelFormat.Format4bppIndexed,
+                           "Invalid BMP - must be 640x480 16-colour.");
+
+                    BitmapData rawData = bitmap.LockBits(new Rectangle(0, 0, BitmapWidth, BitmapHeight), ImageLockMode.ReadOnly, PixelFormat.Format4bppIndexed);
+                    bitmapData = new byte[rawData.Stride * BitmapHeight];
+                    Marshal.Copy(rawData.Scan0, bitmapData, 0, rawData.Stride * BitmapHeight);
+                    bitmap.UnlockBits(rawData);
+
+                    using (var paletteFile = new FileStream(Path.Combine(outputDirectory, "000.dat"), FileMode.Create, FileAccess.Write))
+                    {
+                        ColorPalette palette = bitmap.Palette;
+                        foreach (Color colour in palette.Entries)
+                        {
+                            int R = colour.R / 8;
+                            int G = colour.G / 8;
+                            int B = colour.B / 8;
+                            paletteFile.WriteUShort((ushort)((B << 10) + (G << 5) + R));
+                        }
+                    }
+                }
+            }
+
+            for (int fileNumber = 1; fileNumber < 7; fileNumber++)
+            {
+                using (var file = new FileStream(Path.Combine(outputDirectory, $"{fileNumber:D3}.dat"), FileMode.Create, FileAccess.Write))
+                {
+                    (int tileWidth, int tileHeight, int startX, int startY) = GetTileInfo(fileNumber);
+                    for (int y = startY; y < startY + tileHeight; y++)
+                    {
+                        for (int x = startX; x < startX + tileWidth; x++)
+                        {
+                            byte pixelPair = bitmapData[x + (y * BitmapWidth / 2)];
+                            file.WriteByte(SwapByteHalves(pixelPair));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static (int tileWidth, int tileHeight, int startX, int startY) GetTileInfo(int fileNumber)
+        {
+            int tileWidth = fileNumber % 3 == 0 ? RightColumnWidth : ColumnWidth;
+            int tileHeight = fileNumber > 3 ? BottomRowHeight : TopRowHeight;
+            int startX = (fileNumber - 1) % 3 * ColumnWidth;
+            int startY = fileNumber > 3 ? TopRowHeight : 0;
+            return (tileWidth, tileHeight, startX, startY);
+        }
+
         private static byte SwapByteHalves(byte input) => (byte)(((input & 0xF) << 4) + ((input >> 4) & 0xF));
 
-        private static void Assert(bool condition, string message = null) {
+        private static void Assert(bool condition, string message = null)
+        {
             if (!condition)
             {
                 throw new Exception(message ?? "Assertion failed.");
