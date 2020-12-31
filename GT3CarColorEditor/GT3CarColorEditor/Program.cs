714a1c9761c5a503d802b03c0aaabd75711ceeb4
pez2k <pez2k@pez2k.net>
2020-12-31 16:49:26 +0000

    GT3CarColorEditor: Implement basic mode selection based on the command-line arguments.

--- GT3CarColorEditor/GT3CarColorEditor/Program.cs
+++ GT3CarColorEditor/GT3CarColorEditor/Program.cs
@@ -1,12 +1,34 @@
-﻿namespace GT3.CarColorEditor
+﻿using System;
+
+namespace GT3.CarColorEditor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var data = new CarAndColourList();
-            data.ReadFromGameFiles();
-            data.WriteToCSV();
+            if (args.Length == 1)
+            {
+                var data = new CarAndColourList();
+                if (args[0].EndsWith("db"))
+                {
+                    data.ReadFromGameFiles();
+                    data.WriteToCSV();
+                    return;
+                }
+                else if (args[0].EndsWith(".csv"))
+                {
+                    data.ReadFromCSV();
+                    data.WriteToGameFiles();
+                    return;
+                }
+                else if (args[0] == "-wiki")
+                {
+                    data.ReadFromGameFiles();
+                    data.WriteToWikiText();
+                    return;
+                }
+            }
+            Console.WriteLine("Usage: GT3CarColorEditor carcolor.db\r\nOR\r\nGT3CarColorEditor Cars.csv");
         }
     }
 }
