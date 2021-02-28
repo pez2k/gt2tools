e519a50c7e88a2d4d413d741da540cee275bb577
pez2k <pez2k@pez2k.net>
2021-02-28 17:38:16 +0000

    ISOLayerMerge - Quick tool to merge two ISO layers together into a dual-layer image.

---
+++ ISOLayerMerge/ISOLayerMerge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ISOLayerMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: ISOLayerMerge firstLayer.iso secondLayer.iso mergedOutputFile.iso");
+            }
+            else if (!File.Exists(args[0]) || !File.Exists(args[1]))
+            {
+                Console.WriteLine("Could not open one of the input files.");
+            }
+
+            using (var output = new FileStream(args[2], FileMode.Create, FileAccess.Write))
+            {
+                using (var firstLayer = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+                {
+                    firstLayer.CopyTo(output);
+                }
+                using (var secondLayer = new FileStream(args[1], FileMode.Open, FileAccess.Read))
+                {
+                    secondLayer.Position = 0x8000;
+                    secondLayer.CopyTo(output);
+                }
+            }
+        }
+    }
+}
