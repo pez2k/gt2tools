dbbb2ac66be95e0184f2d358c6c7f685d836fbfd
pez2k <pez2k@pez2k.net>
2022-02-02 15:18:38 +0000

    GT2DataSplitter: Remove leftover properties from DataFile.

--- GT2DataSplitter/GT2DataSplitter/DataStructures/DataFile.cs
+++ GT2DataSplitter/GT2DataSplitter/DataStructures/DataFile.cs
@@ -13,38 +13,6 @@ namespace GT2.DataSplitter
     {
         private readonly TypedData[] data;
 
-        public List<Brake> BrakeParts { get; set; } = new List<Brake>();
-        public List<BrakeController> BrakeControllerParts { get; set; } = new List<BrakeController>();
-        public List<Steer> SteerParts { get; set; } = new List<Steer>();
-        public List<Chassis> ChassisParts { get; set; } = new List<Chassis>();
-        public List<Lightweight> LightweightParts { get; set; } = new List<Lightweight>();
-        public List<RacingModify> RacingModifyParts { get; set; } = new List<RacingModify>();
-        public List<Engine> EngineParts { get; set; } = new List<Engine>();
-        public List<PortPolish> PortPolishParts { get; set; } = new List<PortPolish>();
-        public List<EngineBalance> EngineBalanceParts { get; set; } = new List<EngineBalance>();
-        public List<Displacement> DisplacementParts { get; set; } = new List<Displacement>();
-        public List<Computer> ComputerParts { get; set; } = new List<Computer>();
-        public List<NATune> NATuneParts { get; set; } = new List<NATune>();
-        public List<TurbineKit> TurbineKitParts { get; set; } = new List<TurbineKit>();
-        public List<Drivetrain> DrivetrainParts { get; set; } = new List<Drivetrain>();
-        public List<Flywheel> FlywheelParts { get; set; } = new List<Flywheel>();
-        public List<Clutch> ClutchParts { get; set; } = new List<Clutch>();
-        public List<PropellerShaft> PropellerShaftParts { get; set; } = new List<PropellerShaft>();
-        public List<Gear> GearParts { get; set; } = new List<Gear>();
-        public List<Suspension> SuspensionParts { get; set; } = new List<Suspension>();
-        public List<Intercooler> IntercoolerParts { get; set; } = new List<Intercooler>();
-        public List<Muffler> MufflerParts { get; set; } = new List<Muffler>();
-        public List<LSD> LSDParts { get; set; } = new List<LSD>();
-        public List<TiresFront> TiresFrontParts { get; set; } = new List<TiresFront>();
-        public List<TiresRear> TiresRearParts { get; set; } = new List<TiresRear>();
-        public List<TireSize> TireSizes { get; set; } = new List<TireSize>();
-        public List<TireCompound> TireCompounds { get; set; } = new List<TireCompound>();
-        public List<TireForceVol> TireForceVols { get; set; } = new List<TireForceVol>();
-        public List<ActiveStabilityControl> ActiveStabilityControlParts { get; set; } = new List<ActiveStabilityControl>();
-        public List<TractionControlSystem> TractionControlSystemParts { get; set; } = new List<TractionControlSystem>();
-        public List<CarUnknown> Unknown { get; set; } = new List<CarUnknown>();
-        public List<Car> Cars { get; set; } = new List<Car>();
-        
         public DataFile(params (Type type, int orderOnDisk)[] dataDefinitions)
         {
             data = dataDefinitions.Select(definition => new TypedData(definition.type, definition.orderOnDisk)).ToArray();
