63036ff9c89ca06fd1304378925d9924d0df33cf
pez2k <pez2k@pez2k.net>
2023-03-10 21:59:05 +0000

    GT1DataSplitter: Map Gear to CSV.

--- GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Gear.cs
+++ GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Gear.cs
@@ -1,14 +1,97 @@
-﻿namespace GT1.DataSplitter
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using CsvHelper.Configuration;
+
+namespace GT1.DataSplitter
 {
-    public class Gear : DataStructure
+    using TypeConverters;
+
+    public class Gear : CsvDataStructure<GearData, GearCSVMap>
     {
         public Gear()
         {
             Header = "GEAR";
-            Size = 0x44;
-            // 0x32: car ID
+            StringTableCount = 2;
         }
 
-        protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0x32);
+        protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0x32).Replace(".dat", ".csv");
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x44
+    public struct GearData
+    {
+        public byte NumberOfGears;
+        public byte StageDuplicate;
+        public ushort FirstGearRatioDefault;
+        public ushort SecondGearRatioDefault;
+        public ushort ThirdGearRatioDefault;
+        public ushort FourthGearRatioDefault;
+        public ushort FifthGearRatioDefault;
+        public ushort SixthGearRatioDefault;
+        public ushort SeventhGearRatioDefault;
+        public ushort FinalDriveRatioDefault;
+        public ushort FirstGearRatioMin;
+        public ushort FirstGearRatioMax;
+        public ushort SecondGearRatioMin;
+        public ushort SecondGearRatioMax;
+        public ushort ThirdGearRatioMin;
+        public ushort ThirdGearRatioMax;
+        public ushort FourthGearRatioMin;
+        public ushort FourthGearRatioMax;
+        public ushort FifthGearRatioMin;
+        public ushort FifthGearRatioMax;
+        public ushort SixthGearRatioMin;
+        public ushort SixthGearRatioMax;
+        public ushort SeventhGearRatioMin;
+        public ushort SeventhGearRatioMax;
+        public ushort FinalDriveRatioMin;
+        public ushort FinalDriveRatioMax;
+        public ushort CarID;
+        public ushort Stage;
+        public ushort Padding;
+        public uint Price;
+        public ushort NamePart1;
+        public ushort Padding2;
+        public ushort NamePart2;
+        public ushort UnknownAlways1;
+    }
+
+    public sealed class GearCSVMap : ClassMap<GearData>
+    {
+        public GearCSVMap(List<List<string>> tables)
+        {
+            Map(m => m.NumberOfGears);
+            Map(m => m.StageDuplicate);
+            Map(m => m.FirstGearRatioDefault);
+            Map(m => m.SecondGearRatioDefault);
+            Map(m => m.ThirdGearRatioDefault);
+            Map(m => m.FourthGearRatioDefault);
+            Map(m => m.FifthGearRatioDefault);
+            Map(m => m.SixthGearRatioDefault);
+            Map(m => m.SeventhGearRatioDefault);
+            Map(m => m.FinalDriveRatioDefault);
+            Map(m => m.FirstGearRatioMin);
+            Map(m => m.FirstGearRatioMax);
+            Map(m => m.SecondGearRatioMin);
+            Map(m => m.SecondGearRatioMax);
+            Map(m => m.ThirdGearRatioMin);
+            Map(m => m.ThirdGearRatioMax);
+            Map(m => m.FourthGearRatioMin);
+            Map(m => m.FourthGearRatioMax);
+            Map(m => m.FifthGearRatioMin);
+            Map(m => m.FifthGearRatioMax);
+            Map(m => m.SixthGearRatioMin);
+            Map(m => m.SixthGearRatioMax);
+            Map(m => m.SeventhGearRatioMin);
+            Map(m => m.SeventhGearRatioMax);
+            Map(m => m.FinalDriveRatioMin);
+            Map(m => m.FinalDriveRatioMax);
+            Map(m => m.CarID);
+            Map(m => m.Stage);
+            Map(m => m.Price);
+            Map(m => m.NamePart1).TypeConverter(new StringTableLookup(tables[0]));
+            Map(m => m.NamePart2).TypeConverter(new StringTableLookup(tables[1]));
+            Map(m => m.UnknownAlways1);
+        }
     }
 }
