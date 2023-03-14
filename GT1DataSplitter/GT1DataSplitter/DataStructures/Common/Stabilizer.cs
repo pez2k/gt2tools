7b43ef5ed96d7c80205a47d594edeb8d34def02d
pez2k <pez2k@pez2k.net>
2023-03-14 14:46:12 +0000

    GT1DataSplitter: Map StabilizerFront and StabilizerRear to CSV.

--- GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Stabilizer.cs
+++ GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Stabilizer.cs
@@ -1,14 +1,54 @@
-﻿namespace GT1.DataSplitter
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using CsvHelper.Configuration;
+
+namespace GT1.DataSplitter
 {
-    public abstract class Stabilizer : DataStructure
+    using TypeConverters;
+
+    public abstract class Stabilizer : CsvDataStructure<StabilizerData, StabilizerCSVMap>
     {
         public Stabilizer()
         {
             Header = "STABILZ";
-            Size = 0x14;
-            // 0x4: car ID
+            StringTableCount = 2;
         }
 
         protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0x4);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x14
+    public struct StabilizerData
+    {
+        public byte Default;
+        public byte Steps;
+        public byte Min;
+        public byte Max;
+        public ushort CarID;
+        public byte Stage;
+        public byte StageDuplicate;
+        public uint Price;
+        public ushort NamePart1;
+        public ushort Padding3;
+        public ushort NamePart2;
+        public ushort UnknownAlways1;
+    }
+
+    public sealed class StabilizerCSVMap : ClassMap<StabilizerData>
+    {
+        public StabilizerCSVMap(List<List<string>> tables)
+        {
+            Map(m => m.Default);
+            Map(m => m.Steps);
+            Map(m => m.Min);
+            Map(m => m.Max);
+            Map(m => m.CarID);
+            Map(m => m.Stage);
+            Map(m => m.StageDuplicate);
+            Map(m => m.Price);
+            Map(m => m.NamePart1).TypeConverter(new StringTableLookup(tables[0]));
+            Map(m => m.NamePart2).TypeConverter(new StringTableLookup(tables[1]));
+            Map(m => m.UnknownAlways1);
+        }
     }
 }
