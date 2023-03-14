e8d8201f53788ed01f48b99bd1c6533a48f25ef1
pez2k <pez2k@pez2k.net>
2023-03-14 11:32:57 +0000

    GT1DataSplitter: Map Brake and BrakeController to CSV.

--- GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Brake.cs
+++ GT1DataSplitter/GT1DataSplitter/DataStructures/Common/Brake.cs
@@ -1,14 +1,51 @@
-﻿namespace GT1.DataSplitter
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using CsvHelper.Configuration;
+
+namespace GT1.DataSplitter
 {
-    public class Brake : DataStructure
+    using TypeConverters;
+
+    public class Brake : CsvDataStructure<BrakeData, BrakeCSVMap>
     {
         public Brake()
         {
             Header = "BRAKE";
-            Size = 0x18;
-            // 0x4: car ID
+            StringTableCount = 2;
         }
 
-        protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0x4);
+        protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0x4).Replace(".dat", ".csv");
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x18
+    public struct BrakeData
+    {
+        public byte Unknown;
+        public byte Unknown2;
+        public byte Unknown3;
+        public byte Padding;
+        public ushort CarID;
+        public ushort Padding2;
+        public uint Price;
+        public ushort NamePart1;
+        public ushort Padding3;
+        public ushort NamePart2;
+        public ushort UnknownAlways1;
+        public uint Padding4;
+    }
+
+    public sealed class BrakeCSVMap : ClassMap<BrakeData>
+    {
+        public BrakeCSVMap(List<List<string>> tables)
+        {
+            Map(m => m.Unknown);
+            Map(m => m.Unknown2);
+            Map(m => m.Unknown3);
+            Map(m => m.CarID);
+            Map(m => m.Price);
+            Map(m => m.NamePart1).TypeConverter(new StringTableLookup(tables[0]));
+            Map(m => m.NamePart2).TypeConverter(new StringTableLookup(tables[1]));
+            Map(m => m.UnknownAlways1);
+        }
     }
 }
--- GT1DataSplitter/GT1DataSplitter/DataStructures/Common/BrakeController.cs
+++ GT1DataSplitter/GT1DataSplitter/DataStructures/Common/BrakeController.cs
@@ -1,14 +1,60 @@
-﻿namespace GT1.DataSplitter
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using CsvHelper.Configuration;
+
+namespace GT1.DataSplitter
 {
-    public class BrakeController : DataStructure
+    using TypeConverters;
+
+    public class BrakeController : CsvDataStructure<BrakeControllerData, BrakeControllerCSVMap>
     {
         public BrakeController()
         {
             Header = "BRKCTRL";
-            Size = 0x1C;
-            // 0xA: car ID
+            StringTableCount = 2;
         }
 
         protected override string CreateOutputFilename() => CreateDetailedOutputFilename(0xA);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)] // 0x1C
+    public struct BrakeControllerData
+    {
+        public byte FrontDefault;
+        public byte RearDefault;
+        public byte FrontMin;
+        public byte FrontMax;
+        public byte FrontSteps;
+        public byte RearMin;
+        public byte RearMax;
+        public byte RearSteps;
+        public ushort Padding;
+        public ushort CarID;
+        public uint Padding2;
+        public uint Price;
+        public ushort NamePart1;
+        public ushort Padding3;
+        public ushort NamePart2;
+        public ushort UnknownAlways1;
+    }
+
+    public sealed class BrakeControllerCSVMap : ClassMap<BrakeControllerData>
+    {
+        public BrakeControllerCSVMap(List<List<string>> tables)
+        {
+            Map(m => m.FrontDefault);
+            Map(m => m.RearDefault);
+            Map(m => m.FrontMin);
+            Map(m => m.FrontMax);
+            Map(m => m.FrontSteps);
+            Map(m => m.RearMin);
+            Map(m => m.RearMax);
+            Map(m => m.RearSteps);
+            Map(m => m.CarID);
+            Map(m => m.Price);
+            Map(m => m.NamePart1).TypeConverter(new StringTableLookup(tables[0]));
+            Map(m => m.NamePart2).TypeConverter(new StringTableLookup(tables[1]));
+            Map(m => m.UnknownAlways1);
+        }
     }
 }
