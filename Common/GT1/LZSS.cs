dcd4d6dae28ff96a104d31eed7e3796561418c80
pez2k <pez2k@pez2k.net>
2023-05-02 14:43:50 +0100

    Common.GT1: Add non-functional attempt at LZSS compression from several years ago for posterity.

--- Common/GT1/LZSS.cs
+++ Common/GT1/LZSS.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using StreamExtensions;
 
 namespace GT1.LZSS
@@ -102,6 +103,114 @@ namespace GT1.LZSS
                 }
                 compressed.WriteByte(input.ReadSingleByte());
             }
+        }
+
+        public static void BrokenCompress(Stream input, Stream compressed)
+        {
+            const byte NoCompressionFlags = 0;
+            long lastFlagsPosition = 0;
+            byte flagsWritten = 8;
+            byte compressionFlags = NoCompressionFlags;
+            for (long i = 0; i < input.Length; i++)
+            {
+                if (flagsWritten >= 8)
+                {
+                    compressed.Position = lastFlagsPosition;
+                    compressed.WriteByte(ReverseBits(compressionFlags));
+
+                    if (compressed.Length > 1)
+                    {
+                        compressed.Position = compressed.Length;
+                        lastFlagsPosition = compressed.Position;
+                        compressed.WriteByte(0);
+                    }
+                    flagsWritten = 0;
+                }
+
+                var nextPattern = new byte[3]; // abort search if end of pattern > end of input
+                input.Position = i;
+                input.Read(nextPattern);
+
+                long skipForwardTo = 0;
+                long startOfPattern = -1;
+                ushort patternLength = 0;
+                for (long lookBackPosition = i - 1; lookBackPosition > 0; lookBackPosition--) // abort search if run out of file
+                {
+                    input.Position = lookBackPosition;
+                    var currentPattern = new byte[3];
+                    input.Read(currentPattern);
+
+                    if (currentPattern.SequenceEqual(nextPattern))
+                    {
+                        // start looking for a longer match here
+
+                        startOfPattern = lookBackPosition;
+                        patternLength = 3;
+
+                        if (lookBackPosition + patternLength > i)
+                        {
+                            skipForwardTo = lookBackPosition + patternLength;
+                        }
+                        break;
+                    }
+
+                    if (lookBackPosition == i - 32768) // end of window, abort
+                    {
+                        break;
+                    }
+                }
+
+                if (startOfPattern == -1)
+                {
+                    compressed.WriteByte(nextPattern[0]); // failed to find a match, just write the current byte
+                }
+                else
+                {
+                    compressed.WriteByte((byte)(patternLength - 3));
+
+                    ushort lookBackDistance = (ushort)(i - startOfPattern - 1);
+
+                    if (lookBackDistance >= MultiByteDistanceFlag)
+                    {
+                        byte topByte = (byte)(lookBackDistance / 256);
+                        compressed.WriteByte((byte)(topByte | MultiByteDistanceFlag));
+                        compressed.WriteByte((byte)lookBackDistance);
+                    }
+                    else
+                    {
+                        compressed.WriteByte((byte)lookBackDistance);
+                    }
+                    compressionFlags |= CompressedFlag;
+                }
+
+                if (flagsWritten < 7)
+                {
+                    compressionFlags <<= 1;
+                }
+                flagsWritten++;
+
+                if (skipForwardTo > 0)
+                {
+                    i = skipForwardTo;
+                }
+            }
+        }
+
+        private static byte ReverseBits(byte input)
+        {
+            byte output = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                output |= (byte)(input & 1);
+                if (i < 7)
+                {
+                    input >>= 1;
+                    output <<= 1;
+                }
+            }
+
+            return output;
         }
     }
 }
