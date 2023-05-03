c9a78475f61705538a8fb8510f665e4534253587
pez2k <pez2k@pez2k.net>
2023-05-03 16:09:38 +0100

    GT1Compress: Allow passing in the compression level (window size in KB).

--- GT1Compress/GT1Compress/Program.cs
+++ GT1Compress/GT1Compress/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace GT1.Compress
 {
@@ -8,16 +9,32 @@ namespace GT1.Compress
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length == 0 || args.Length > 2)
             {
+                Console.WriteLine("Usage:\r\nGT1Compress <filename>\r\nOR\r\nGT1Compress <compression level 1 - 32> <filename>\r\n\r\ne.g.: GT1Compress 4 tsplr.tex");
                 return;
             }
 
-            using (FileStream input = new FileStream(args[0], FileMode.Open))
+            string filename;
+            int windowSize = 1024;
+            if (args.Length == 2)
             {
-                using (FileStream output = new FileStream(args[0] + "_compressed", FileMode.Create))
+                if (int.TryParse(args[0], out int compressionLevel) && compressionLevel > 0 && compressionLevel <= 32)
                 {
-                    LZSS.Compress(input, output);
+                    windowSize = compressionLevel * 1024;
+                }
+                filename = args[1];
+            }
+            else
+            {
+                filename = args[0];
+            }
+
+            using (FileStream input = new FileStream(filename, FileMode.Open))
+            {
+                using (FileStream output = new FileStream($"{filename}_compressed", FileMode.Create))
+                {
+                    LZSS.Compress(input, output, windowSize);
                 }
             }
         }
