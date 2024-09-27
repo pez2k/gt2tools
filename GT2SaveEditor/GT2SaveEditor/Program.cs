b7ae81ddb51d1a138a9afa1c8b3173d2cb70b369
pez2k <pez2k@pez2k.net>
2024-09-27 14:47:20 +0100

    GT2SaveEditor: Move the save file loading and writing into a handler class that can be shared by a GUI tool.

--- GT2SaveEditor/GT2SaveEditor/Program.cs
+++ GT2SaveEditor/GT2SaveEditor/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.IO;
-using Force.Crc32;
-using StreamExtensions;
 
 namespace GT2.SaveEditor
 {
@@ -12,48 +10,16 @@ namespace GT2.SaveEditor
             {
                 int blockNumber = 1;
                 int offset = blockNumber * 0x2000;
-                SaveFile save = ReadSave(file, offset);
+                SaveFile save = SaveFileHandler.ReadSave(file, offset);
 
                 using (FileStream outfile = new("gt2saveeditor.mcr", FileMode.OpenOrCreate, FileAccess.ReadWrite))
                 {
                     file.Position = 0;
                     file.CopyTo(outfile);
 
-                    outfile.Position = offset;
-                    for (int i = 0; i < (0x2000 * 4); i++) // Blank the 4 blocks in the memory card for a clean write
-                    {
-                        outfile.WriteByte(0);
-                    }
-
-                    WriteSave(outfile, offset, save);
+                    SaveFileHandler.WriteSave(outfile, offset, save);
                 }
             }
-        }
-
-        static SaveFile ReadSave(Stream file, int offset)
-        {
-            file.Position = offset;
-            SaveFile save = new();
-            save.ReadFromSave(file);
-            return save;
-        }
-
-        static void WriteSave(Stream file, int offset, SaveFile save)
-        {
-            file.Position = offset;
-            save.WriteToSave(file);
-            ChecksumSave(file, offset);
-        }
-
-        static void ChecksumSave(Stream file, int offset)
-        {
-            int start = offset; // Includes all header frames
-            int end = offset + 0x7E9C; // Entire save data until the CRC32 checksum itself
-            file.Position = start;
-            byte[] buffer = new byte[end - start];
-            file.Read(buffer);
-            uint checksum = Crc32Algorithm.Compute(buffer);
-            file.WriteUInt(checksum);
         }
     }
 }
---
+++ GT2SaveEditor/GT2SaveEditor/SaveFileHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Force.Crc32;
+using StreamExtensions;
+
+namespace GT2.SaveEditor
+{
+    public static class SaveFileHandler
+    {
+        public static SaveFile OpenSave(string filename)
+        {
+            using (FileStream file = new(filename, FileMode.Open, FileAccess.Read))
+            {
+                int blockNumber = 1;
+                int offset = blockNumber * 0x2000;
+                return ReadSave(file, offset);
+            }
+        }
+
+        public static SaveFile ReadSave(Stream file, int offset)
+        {
+            file.Position = offset;
+            SaveFile save = new();
+            save.ReadFromSave(file);
+            return save;
+        }
+
+        public static void WriteSave(string filename, SaveFile save)
+        {
+            using (FileStream outfile = new(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                int blockNumber = 1;
+                int offset = blockNumber * 0x2000;
+                WriteSave(outfile, offset, save);
+            }
+        }
+
+        public static void WriteSave(Stream file, int offset, SaveFile save)
+        {
+            WipeBlocks(file, offset);
+            file.Position = offset;
+            save.WriteToSave(file);
+            ChecksumSave(file, offset);
+        }
+
+        public static void WipeBlocks(Stream file, int offset)
+        {
+            file.Position = offset;
+            for (int i = 0; i < (0x2000 * 4); i++) // Blank the 4 blocks in the memory card for a clean write
+            {
+                file.WriteByte(0);
+            }
+        }
+
+        public static void ChecksumSave(Stream file, int offset)
+        {
+            int start = offset; // Includes all header frames
+            int end = offset + 0x7E9C; // Entire save data until the CRC32 checksum itself
+            file.Position = start;
+            byte[] buffer = new byte[end - start];
+            file.Read(buffer);
+            uint checksum = Crc32Algorithm.Compute(buffer);
+            file.WriteUInt(checksum);
+        }
+    }
+}
